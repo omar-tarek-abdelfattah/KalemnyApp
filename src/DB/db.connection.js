162fc206923f902012743ed395c752d47c542971
omar-tarek-abdelfattah <omartarek77124@gmail.com>
2025-10-07 01:29:12 +0300

    add morgan

--- src/DB/db.connection.js
+++ src/DB/db.connection.js
@@ -4,8 +4,10 @@ const connectDB = async () => {
     const atlasConnection = process.env.DB_ATLAS_URI
     const localConnection = process.env.DB_LOCAL_URI
 
+    console.log(atlasConnection);
     try {
         const result = await mongoose.connect(`${atlasConnection}`)
+        
         console.log(`db connected !`);
 
     } catch (error) {
--- src/app.controller.js
+++ src/app.controller.js
@@ -2,7 +2,7 @@ import path from "node:path"
 import * as dotenv from 'dotenv'
 // dotenv.config({ path: path.join("./src/config/.env.dev"), quiet: true })
 // dotenv.config({ path: path.join("./src/config/.env.prod"), quiet: true })
-dotenv.config({})
+dotenv.config()
 
 import express from 'express'
 import authController from './modules/auth/auth.controller.js'
